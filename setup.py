189177b576d5bededf1a5b33ac0889bd8509abdf
oncowonyoungcho <143578451+oncowonyoungcho@users.noreply.github.com>
2023-11-20 22:21:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from dunamai import Version, Style
 
 setup(
     name="nii2dcm",
-    version=Version.from_git().serialize(metadata=False, style=Style.SemVer),
+    version="0.1.2"#Version.from_git().serialize(metadata=False, style=Style.SemVer),
 )
