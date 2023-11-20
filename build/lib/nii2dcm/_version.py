4c42b50257693c0842f6183e3c37977ff18131f3
oncowonyoungcho <143578451+oncowonyoungcho@users.noreply.github.com>
2023-11-20 22:25:07 +0900

    Update _version.py

--- build/lib/nii2dcm/_version.py
+++ build/lib/nii2dcm/_version.py
@@ -1,2 +1,2 @@
 from dunamai import Version, Style
-__version__ = Version.from_git().serialize(metadata=False, style=Style.SemVer)
+__version__ = '0.1.2'#Version.from_git().serialize(metadata=False, style=Style.SemVer)
